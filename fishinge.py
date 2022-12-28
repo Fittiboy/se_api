675aae8e98ee09b0162cc245e43edc9bec573e85
Fitti <dev@fitti.io>
2022-12-28 19:31:34 +0100

    Get fishinge command

--- fishinge.py
+++ fishinge.py
@@ -9,18 +9,28 @@ headers_base = {
 }
 
 
-def get_channels(jwt, headers):
+def get_channel_id(jwt, headers, channel_name):
     url = url_base + "users/access"
     r = requests.get(url=url, headers=headers).json()
-    pprint(r)
+    for result in r:
+        if result["username"] == channel_name:
+            return result["channelId"]
 
 
-def get_commands(jwt, headers, channel_id):
+def get_command(jwt, headers, channel_id, command_name):
     url = url_base + f"bot/commands/{channel_id}"
+    r = requests.get(url=url, headers=headers).json()
+    for result in r:
+        if result["command"] == command_name:
+            return result
 
 
 if __name__ == "__main__":
     jwt = input("JWT: ").strip("\"")
-    headers_updated = headers_base
-    headers_updated["Authorization"] = f"Bearer {jwt}"
-    get_channels(jwt, headers_updated)
+    headers = headers_base
+    headers["Authorization"] = f"Bearer {jwt}"
+    channel_name = "chobo"
+    command_name = "fishinge"
+    channel_id = get_channel_id(jwt, headers, channel_name)
+    command = get_command(jwt, headers, channel_id, command_name)
+    pprint(command)
